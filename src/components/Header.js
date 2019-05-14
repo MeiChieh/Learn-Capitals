29f4adb94fb189c3dc984020fcdb6fe00a2e2c61
MeiCC <meichieh.chien@gmail.com>
2019-05-15 00:56:57 +0200

    change fonts

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./App.scss";
+import earth from "../media/earth5.svg";
 
 const Header = () => {
   return (
     <div className="app-header">
-      <h2 className="ui header">
+      <span className="header-text">Learn World Capitals</span>
+      <img className="header-img" src={earth} alt="" />
+      {/* <div className='headerImg'></div> */}
+      {/* <h2 className="ui header">
         <i className="flag icon" />
         <div className="content">Learn World Countries</div>
-      </h2>
+      </h2> */}
     </div>
   );
 };
