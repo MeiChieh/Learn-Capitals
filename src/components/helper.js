bf64dae40b47de360080974bb83f9b7cdbc5d587
MeiCC <meichieh.chien@gmail.com>
2019-05-17 00:23:43 +0200

    helper added

---
+++ src/components/helper.js
@@ -0,0 +1,10 @@
+// firstChat
+const firstChat = {
+  firstChatStyle: {
+    paddingBottom: "4vh"
+  },
+  firstChatMessage:
+    "Hello, welcome to learn world countries. I am Roboto, let's prepare you for the next Irish bar drinking game. I will give you the name of the country, and you have to tell me where it's capital is. If you are not sure about where this country is, you can click on the map icon and have a look."
+};
+
+export default firstChat;
