81be2fffe7916f22a73d2c1bb076f2d1448f92ff
MeiCC <meichieh.chien@gmail.com>
2019-05-16 14:43:38 +0200

    push question successful

--- src/components/ChatboxLayout.js
+++ src/components/ChatboxLayout.js
@@ -1,45 +1,51 @@
 import React from "react";
 import ChatboxLeft from "./ChatboxLeft";
-import ChatboxRight from "./ChatboxRight";
+// import ChatboxLeft from "./ChatboxLeft";
+// import ChatboxRight from "./ChatboxRight";
 
 class ChatboxLayout extends React.Component {
-  // props = props.askQuestion & props.gameRecord
-  // constructor() {
-  //   super();
-  //   // this.gameRecord = this.props.gameRecord;
-  // }
-
   state = {};
-  askCountry = () => {
-    if (this.props.gameRecord.length == 0) {
-      return;
-    }
-    for (let item of this.props.gameRecord) {
-      console.log(item.country);
-      console.log(item.country);
-      console.log(item.country);
-      console.log(item.country);
-    }
-  };
+  // askCountry = () => {
+  //   if (this.props.gameRecord.length == 0) {
+  //     return;
+  //   }
+  //   for (let item of this.props.gameRecord) {
+  //     console.log(item.country);
+  //   }
+  // };
 
   render() {
-    // console.log(gameRecord.length);
-    this.askCountry();
-    const { askQuestion, gameRecord } = this.props;
-    return <div className="chatbox-layout" />;
+    const children = [];
+    const { gameRecord } = this.props;
+
+    for (let item of gameRecord) {
+      if (gameRecord.length == 0) {
+        return;
+      }
+      const { country } = item;
+
+      // function pushIt() {
+      //   setTimeout(() => {
+      children.push(
+        <ChatboxLeft message={`Where is the capital of ${country} ?`} />
+      );
+      //   }, 1000);
+      // }
+    }
+
+    return <div className="chatbox-layout">{children}</div>;
   }
+
+  // if (askQuestion == true) {
+  //   console.log("ask it");
+  // }
+
+  // for (let item of gameRecord) {
+  //   return (
+  //     <div className="chatbox-layout">
+  //       <ChatboxLeft message={`Where is the capital of ${item.country}`} />
+  //     </div>
+  //   );
+  // }
 }
-
-// if (askQuestion == true) {
-//   console.log("ask it");
-// }
-
-// for (let item of gameRecord) {
-//   return (
-//     <div className="chatbox-layout">
-//       <ChatboxLeft message={`Where is the capital of ${item.country}`} />
-//     </div>
-//   );
-// }
-
 export default ChatboxLayout;
